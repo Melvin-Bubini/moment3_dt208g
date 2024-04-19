b77a582fb2289bca16067d596d6b18df233841d2
Melvin Bubini <melvin.bubini@gmail.com>
2024-04-19 10:31:13 +0200

    andra utkast för klar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,6 +8,36 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  openBtn: HTMLElement | null = null;
+closeBtn: HTMLElement | null = null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.openBtn = document.getElementById("open-menu");
+    this.closeBtn = document.getElementById("close-menu");
+
+    if (this.openBtn && this.closeBtn) {
+      this.openBtn.addEventListener("click", this.toggleMenu);
+      this.closeBtn.addEventListener("click", this.toggleMenu);
+    }
+  }
+
+  toggleMenu(): void {
+    let navMenuEl: HTMLElement | null = document.getElementById("nav-menu");
+
+    if (navMenuEl) {
+      let style: CSSStyleDeclaration = window.getComputedStyle(navMenuEl);
+
+      if (style.display === "none") {
+        navMenuEl.style.display = "block";
+      } else {
+        navMenuEl.style.display = "none";
+      }
+    }
+  }
 }
+
+
